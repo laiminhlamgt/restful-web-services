646bd9733ca935ac299e8e80b9b3e0843a70ff72
Lam Lai <laiminhlamgt@gmail.com>
2020-01-14 16:48:57 +0700

    Step 15 - Implementing Validations for RESTful Services

--- src/main/java/com/lammlai/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/lammlai/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,18 @@
 package com.lammlai.rest.webservices.restfulwebservices.user
         ;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
     private Integer id;
+
+    @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
+
+    @Past
     private Date birthDate;
 
     protected User() {
